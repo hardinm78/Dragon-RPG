29b4b8419a14ad86d833a1fa85d6f51b8e337b1f
hardinm78 <michaelhardin@Michaels-iMac.local>
2017-02-28 15:24:50 -0600

    press G to toggle between direct and indirect movement

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D targetCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+
+	[SerializeField] Vector2 cursorHotSpot = new Vector2 (96, 96);
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster> ();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		switch(cameraRaycaster.layerHit){
+		case Layer.Walkable:
+			Cursor.SetCursor (walkCursor, cursorHotSpot, CursorMode.ForceSoftware);
+			break;
+		case Layer.Enemy:
+			Cursor.SetCursor (targetCursor, cursorHotSpot, CursorMode.ForceSoftware);
+			break;
+		case Layer.RaycastEndStop:
+			Cursor.SetCursor (unknownCursor, cursorHotSpot, CursorMode.ForceSoftware);
+			break;
+		default:
+			Debug.Log ("error");
+			return;
+		}
+
+
+
+
+	}
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+	
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-
+	bool isInDirectMovementMode = false;
 	[SerializeField] float walkMoveStopRadius = 0.2f;
 
     private void Start()
@@ -17,10 +18,35 @@ public class PlayerMovement : MonoBehaviour
         m_Character = GetComponent<ThirdPersonCharacter>();
         currentClickTarget = transform.position;
     }
-
+    private void Update(){
+		if (Input.GetKeyDown(KeyCode.G)){			
+			isInDirectMovementMode = !isInDirectMovementMode;
+			print (isInDirectMovementMode);
+		}
+    }
     // Fixed update is called in sync with physics
-    private void FixedUpdate ()
-	{
+    private void FixedUpdate ()	{
+
+		if (isInDirectMovementMode){
+			ProcessDirectMovement ();
+		}else {
+			ProcessMouseMovement ();
+		}		
+    }
+
+	void ProcessDirectMovement (){
+		float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+       Vector3	m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+       Vector3 m_Move = v*m_CamForward + h*Camera.main.transform.right;
+
+		m_Character.Move (m_Move, false, false);
+	}
+
+
+
+	void ProcessMouseMovement () {
 		if (Input.GetMouseButton (0)) {
 			print ("Cursor raycast hit" + cameraRaycaster.layerHit);
 			switch (cameraRaycaster.layerHit) {
@@ -36,17 +62,14 @@ public class PlayerMovement : MonoBehaviour
 				print ("i dont think we are supposed to be here lady");
 				return;
 			}
-
 		}
 		var playerToClickPoint = currentClickTarget - transform.position;
-
 		if (playerToClickPoint.magnitude > walkMoveStopRadius) {
 			m_Character.Move (playerToClickPoint, false, false);
-		}else {
+		}
+		else {
 			m_Character.Move (Vector3.zero, false, false);
 		}
-
-		
-    }
+	}
 }
 
