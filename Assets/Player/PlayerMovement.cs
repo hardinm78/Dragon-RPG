bbf5d4887df41be53e67466f8f01e6cb78e586b0
hardinm78 <michaelhardin@Michaels-iMac.local>
2017-03-08 09:59:10 -0600

    cursor toggles with control mode switch

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -25,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update(){
 		if (Input.GetKeyDown(KeyCode.G)){			
 			isInDirectMovementMode = !isInDirectMovementMode;
+			Cursor.visible = !Cursor.visible;
 			currentDestination = transform.position;
 			//print (isInDirectMovementMode);
 		}
