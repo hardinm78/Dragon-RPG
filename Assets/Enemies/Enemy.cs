6f3f0cc6cae679cf2ae6a0b585e9e785920041a1
hardinm78 <michaelhardin@Michaels-iMac.local>
2017-03-15 11:15:01 -0500

    enemy pathfinding

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
 	[SerializeField] float maxHealthPoints = 100f;
+	[SerializeField] float attackRadius = 4f;
+	GameObject player = null;
 	float currentHealthPoints = 100f;
-
+	AICharacterControl aiCharacterControl = null;
 
 	public float healthAsPercentage{
 		get {
 			return currentHealthPoints / maxHealthPoints;
+		}
+	}
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		aiCharacterControl = GetComponent<AICharacterControl>();
+	}
+
+	void Update(){
+		float distanceToPlayer = Vector3.Distance (player.transform.position, transform.position);
+
+		if (distanceToPlayer<=attackRadius){
+			aiCharacterControl.SetTarget(player.transform);
+		}else{
+			aiCharacterControl.SetTarget(transform);
+
 		}
 	}
 }
